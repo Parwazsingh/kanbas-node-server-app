4ba052a828661d670f1db3e18bb55d6c233533f4
Parwaz Singh Sarao <parwazekam@gmail.com>
2024-04-01 11:57:28 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import Hello from "./Hello.js"
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+AssignmentRoutes(app)
+Lab5(app);
+Hello(app)
+app.listen(process.env.PORT || 4000);
---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,66 @@
+export default[
+    {
+        "_id": "RS101",
+        "name": "CS2567 Machine Learning",
+        "number": "RS4550",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "Machinelearning.jpeg"
+    },
+    {
+        "_id": "RS102",
+        "name": "CS3467 Foundation of AI",
+        "number": "RS4560",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "AI.webp"
+    },
+    {
+        "_id": "RS103",
+        "name": "CS2879 DBMS",
+        "number": "RS4570",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "DBMS.png"
+    },
+    {
+        "_id": "RS104",
+        "name": "CS6789 IDMP",
+        "number": "RS4580",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "IDMP.jpeg"
+    },
+    {
+        "_id": "RS105",
+        "name": "CS6721 Robotics",
+        "number": "RS4590",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "Robotics.png"
+    },
+    {
+        "_id": "RS106",
+        "name": "CS7892 Web Development",
+        "number": "RS4591",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "web-development-2.webp"
+    },
+    {
+        "_id": "RS107",
+        "name": "CS4356 Mental Health",
+        "number": "RS4592",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "Mentalhealth.webp"
+    },
+    {
+        "_id": "RS108",
+        "name": "CS2345 Career Development",
+        "number": "RS4593",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "Carrerdev.jpeg"
+    }
+]
