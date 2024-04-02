934fddadb436762cae38f8d6e5189db9495b5698
Parwaz Singh Sarao <parwazekam@gmail.com>
2024-04-01 20:57:43 -0400

    fourth  commit

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import cors from "cors";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import CourseRoutes from "./Kanbas/courses/routes.js";
-import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import ModuleRoutes from './Kanbas/Modules/routes.js';
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import CourseRoutes from './Kanbas/courses/routes.js';
 
 const app = express();
 app.use(cors());
