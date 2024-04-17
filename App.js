59e6406b741a22bbc456554cb8dbe11c3ccf3994
Parwaz Singh Sarao <parwazekam@gmail.com>
2024-04-17 18:59:09 -0400

    eleven commit

--- App.js
+++ App.js
@@ -10,37 +10,28 @@ import CourseRoutes from './Kanbas/courses/routes.js';
 import mongoose from "mongoose";
 import UserRoutes from './Kanbas/Users/routes.js';
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: 'https://a6--celebrated-blancmange-dbe5fb.netlify.app'
+    origin: process.env.FRONTEND_URL
   }));
   const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    proxy: true,
-    cookie: {
-        sameSite: 'none',
-        secure: true,
-        domain: "https://kanbas-node-server-app-9-v3s7.onrender.com"
-    }
-};
-  
-
-
-  // if (process.env.NODE_ENV !== "development") {
-  //   sessionOptions.proxy = true;
-  //   sessionOptions.cookie = {
-  //     sameSite: "none",
-  //     secure: true,
-  //     domain: process.env.HTTP_SERVER_DOMAIN,
-  //   };
-  // }
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
   app.use(session(sessionOptions));
   
   
